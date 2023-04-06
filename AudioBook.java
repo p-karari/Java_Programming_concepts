a4022506f74330c473c3a73b948715de331ae1c2
p-karari <kararikelvin2002@gmail.com>
2023-04-06 10:02:21 +0300

    implements inheritance

---
+++ AudioBook.java
@@ -0,0 +1,10 @@
+
+public class AudioBook extends book{
+	private int runTime ;
+
+	AudioBook(String title , String author , int runTime) {
+		super(title , author );
+		this.runTime = runTime;
+	}
+
+}
---
+++ Ebook.java
@@ -0,0 +1,9 @@
+
+public class Ebook extends book{
+	private String format ;
+	public Ebook(String title , String author , String format) {
+		super(title , author);
+		this.format = format;
+	}
+
+}
